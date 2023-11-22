94542d858b66ae610c83022c62c22837d5f6156a
Andrey Didenko <ded14181998@gmail.com>
2023-11-22 17:11:06 +0500

    solution

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createElement} from './utils.js';
+import { createElement } from './utils.js';
 import './styles.css';
 
 /**
@@ -7,9 +7,10 @@ import './styles.css';
  * @param store {Store} Хранилище состояния приложения
  * @returns {React.ReactElement}
  */
-function App({store}) {
+function App({ store }) {
 
   const list = store.getState().list;
+  console.log(list)
 
   return (
     <div className='App'>
@@ -24,9 +25,9 @@ function App({store}) {
           list.map(item =>
             <div key={item.code} className='List-item'>
               <div className={'Item' + (item.selected ? ' Item_selected' : '')}
-                   onClick={() => store.selectItem(item.code)}>
+                onClick={() => store.selectItem(item.code)}>
                 <div className='Item-code'>{item.code}</div>
-                <div className='Item-title'>{item.title}</div>
+                <div className='Item-title'>{item.title} {item.count ? `| Выделяли ${item.count} раз` : <></>}</div>
                 <div className='Item-actions'>
                   <button onClick={() => store.deleteItem(item.code)}>
                     Удалить
--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ class Store {
   constructor(initState = {}) {
     this.state = initState;
     this.listeners = []; // Слушатели изменений состояния
+    this.code = Math.max.apply(null, initState.list.map(item => item.code))
   }
 
   /**
@@ -42,9 +43,25 @@ class Store {
    * Добавление новой записи
    */
   addItem() {
+    // const getRandomId = () => {
+    //   const num = Math.floor(Math.random() * 100) // сейчас доступно 100 уникальных id, можно менять на любое количество
+    //   const listItem = this.state.list.find(item => {
+    //     return item.code === num;
+    //   });
+    //   if (!listItem) {
+    //     return num;
+    //   }
+    //   return getRandomId();
+    // }
+
+
+    // не знаю, можно использовать идентификатор после удаления или нет, реализовал оба варианта.
+    const getId = () => {
+      return this.code += 1;
+    }
     this.setState({
       ...this.state,
-      list: [...this.state.list, {code: this.state.list.length + 1, title: 'Новая запись'}]
+      list: [...this.state.list, { code: getId(), title: 'Новая запись' }]
     })
   };
 
@@ -69,6 +86,10 @@ class Store {
       list: this.state.list.map(item => {
         if (item.code === code) {
           item.selected = !item.selected;
+          !item.count ? item.count = 0 : null;
+          item.selected ? item.count += 1 : null;
+        } else {
+          item.selected = false;
         }
         return item;
       })
